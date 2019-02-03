61a82be4f909762105aa0d5a6d6bc9bb6f6b431b
alhormigos <47031436+alhormigos@users.noreply.github.com>
2019-02-03 21:01:24 +0100

    Add files via upload

---
+++ animatedMenu.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $('a[href*=#]').bind('click', function(e) {
+            e.preventDefault(); // prevent hard jump, the default behavior
+
+            var target = $(this).attr("href"); // Set the target as variable
+
+            // perform animated scrolling by getting top-position of target-element and set it as scroll target
+            $('html, body').stop().animate({
+                    scrollTop: $(target).offset().top
+            }, 600, function() {
+                    location.hash = target; //attach the hash (#jumptarget) to the pageurl
+            });
+
+            return false;
+    });
+});
+
+$(window).scroll(function() {
+
+    var scrollDistance = $(window).scrollTop();
+
+    // Assign active class to nav links while scolling
+    $('.page-section').each(function(i) {
+            if ($(this).position().top <= scrollDistance) {
+                    $('.navigation li a.actif').removeClass('actif');
+                    $('.navigation li a').eq(i).addClass('actif');
+
+            }
+    });
+}).scroll();
